a5027debf63e6b2712075203e0392b1957b73d8d
Florindo <flori_89_ld@hotmail.com>
2020-11-26 18:11:38 +0100

    add script

---
+++ clases/florindo/2020-11-25/script.php
@@ -0,0 +1,5 @@
+<?php
+    // docker run -it --rm --name my-running-script -v C:\Users\Florindo\Desktop\FLORINDO\REPOS\IAW:/usr/src/myapp -w /usr/src/myapp php:7.4-cli php clases/florindo/2020-11-25/script.php
+    $archivo = "C:/Users/Florindo/tmp.tmp";
+    print($archivo);
+?>
